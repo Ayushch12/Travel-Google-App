691b7869afb94547c1ca9447e010ed48592f8235
Ayush chalisee <ayushchalise12@gmail.com>
2023-06-11 14:23:40 +0200

    Updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
+
 import React from "react";
-<<<<<<< HEAD
-import ReactDOM from "react-dom";
-
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-=======
 import  ReactDOM  from "react-dom";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementsById("root")) ;
->>>>>>> 26499d8e35be0d37fd855ebd5bbf9361c34bd70f
+ReactDOM.render(<App />, document.getElementById("root"));
+
 
 
