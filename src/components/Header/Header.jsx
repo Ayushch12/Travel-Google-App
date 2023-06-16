fa28284f374939eb3f4398d1c1062abd6ba98731
Ayush chalisee <ayushchalise12@gmail.com>
2023-06-16 15:34:23 +0200

    Map.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Autocomplete } from "@react-google-maps/api";
 import {AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core';
-import SearchIcon from "@material-ui/icons";
+import SearchIcon from "@material-ui/icons/Search";
+
+import useStyles from './styles' ;
+
 
 const Header = ( )=>{
+    const classes = useStyles(); // Add this line to initialize the styles
     return(
 
         <AppBar position="static">
@@ -16,13 +20,17 @@ const Header = ( )=>{
                 <Typography variant="h6" className={classes.title}>
                   Explore new places
                 </Typography>
-                <Autocomplete>
+
+                 <Autocomplete>
                     <div className={classes.search}>
                         <div  className={classes.searchIcon}>
                         <SearchIcon />
                          </div>
+                         <InputBase placeholder="Search ..." classes={{root: classes.inputRoot, input:classes.inputInput  }}/>
+
                     </div>
                 </Autocomplete>
+
                 </Box>
             </Toolbar>
         </AppBar>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,33 @@
-import React from "react";
 
-const Map = ( )=>{
-    return(
-        <h1>Map</h1>
-    );
-}
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
 
-export default Map ;
+import useStyles from './styles';
 
+const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+         bootstrapURLKeys={{ key: 'AIzaSyB6iGfMEkHSjCU0_Gd3MJuM6rqaPt_2YJY' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={null}
+        onChange={null}
+        onChildClick={null}
+      >
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
